223bac779615cce3fdf529f5ad3f4ad8195fe6a9
Ivan Puljić <IvanPuljich955@gmail.com>
2023-06-12 16:51:40 +0200

    add new chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import ArrowLeftCircle from "./assets/arrow-left-circle";
+import "./styles/styles.scss";
+
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="container">
+      <h1>Chat aplikacija</h1>
+      <div className="chat">
+        <header className="chat__header">
+          <ArrowLeftCircle />
+          <div className="chat__header__info"></div>
+        </header>
+      </div>
     </div>
   );
 }
