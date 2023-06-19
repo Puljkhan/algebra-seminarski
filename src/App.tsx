08465c31e146fbd2a70e2cb8622d31b383265076
Ivan Puljić <IvanPuljich955@gmail.com>
2023-06-19 17:30:53 +0200

    add messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
-import ArrowLeftCircle from "./assets/arrow-left-circle";
 import "./styles/styles.scss";
 
 function App() {
   return (
-    <div className="container">
-      <h1>Chat aplikacija</h1>
-      <div className="chat">
-        <header className="chat__header">
-          <ArrowLeftCircle />
-          <div className="chat__header__info"></div>
-        </header>
-      </div>
+    <div>
     </div>
   );
 }
